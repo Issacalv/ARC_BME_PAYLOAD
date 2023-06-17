9a05b35498ee6aa9cbc39d56c4c5d433be967b68
Issacalv <99110653+Issacalv@users.noreply.github.com>
2023-06-17 15:27:04 -0500

    Update ARC_Payload_Code.ino
    Fixed Void Loop running twice

--- ARC_Payload_Code.ino
+++ ARC_Payload_Code.ino
@@ -12,16 +12,12 @@
 #define Red_LED 2
 #define Green_LED 4
 #define chipSelect 10
-
 char currentName[] = "MFC00.txt";
 
 void setup() {
   Serial.begin(9600);
-  analogReadResolution(12);
-  analogReference(AR_INTERNAL2V4);
   pinMode (Red_LED , OUTPUT);
   pinMode(Green_LED, OUTPUT);
-  //while(!Serial);
 
 
  Serial.print("Initializing SD card....");
@@ -35,17 +31,6 @@ void setup() {
   Serial.println("Card is present and initialized");
   digitalWrite(Green_LED , HIGH);
 
-  for (byte i = 1 ; i < 99 ; i++) { // for loop updates a fileName if it exists.
-    if (SD.exists(currentName)){
-      currentName[3] = i/10 + '0';
-      currentName[4] = i%10 + '0';
-    }
-    
-    else{
-      break;
-    }
-  }
-  
   if(!BARO.begin()){
     Serial.println("Failed to initialize pressure sensor!");
     digitalWrite(Red_LED, HIGH);
@@ -54,11 +39,41 @@ void setup() {
     delay(250);
     while(1);
   }
+
+  if(!HTS.begin()){
+    Serial.println("Failed to initialize humidity temperature sensor!");
+    digitalWrite(Red_LED, HIGH);
+    delay(250);
+    digitalWrite(Red_LED, LOW);
+    delay(250);
+    while(1);
+  }
+
+  if (!IMU.begin()){
+    Serial.println("Fialed to initialize IMU!");
+    digitalWrite(Red_LED, HIGH);
+    delay(1500);
+    digitalWrite(Red_LED, LOW);
+    delay(1500);
+    while(1);
+  }
+
+  for (byte i = i ; i < 99 ; i++) {
+    if (SD.exists(currentName)){
+      currentName[3] = i/10 + '0';
+      currentName[4] = i%10 + '0';
+    }
+    else {
+      break;
+    }
+  }
+
 }
 
 
 void loop() {
-  
+
+
   double Altitude_Value = Altitude(Pressure_On);
   double* Acceleration_Value = Acceleration(Acceleration_On);
   double* Gyroscope_Value = Gyroscope(Gyroscope_On);
@@ -66,9 +81,9 @@ void loop() {
   float Humidity_Value = Humidity(Humidity_On);
   //double Voltage_Value = Voltage(R1 , R2);
   float* Voltage_Value = Voltage(Voltage_On);
-  
+
   my_Serial_Monitor(Troubleshoot_On, Altitude_Value, Acceleration_Value, Gyroscope_Value, Temperature_Value, Humidity_Value, Voltage_Value);
-  my_Data_Logger(currentName, Data_On, Altitude_Value, Acceleration_Value, Gyroscope_Value, Temperature_Value, Humidity_Value, Voltage_Value);
-  
+  my_Data_Logger(currentName, Altitude_Value, Acceleration_Value, Gyroscope_Value, Temperature_Value, Humidity_Value, Voltage_Value);
+
  
 }
