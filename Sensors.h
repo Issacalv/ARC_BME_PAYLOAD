bb89e48c9fc98e8c1ac909d80d8670ddf5821ad9
Issacalv <99110653+Issacalv@users.noreply.github.com>
2023-06-17 15:28:23 -0500

    Update Sensors.h

--- Sensors.h
+++ Sensors.h
@@ -15,11 +15,12 @@
 #define V_1_arr 0
 #define V_2_arr 1
 
+
 #define Voltage_Sensor_1 A0
 #define Voltage_Sensor_2 A4
 
 #define Reference_Voltage  0.6
-  
+
 double Altitude (int Pressure_On){
 
   if (Pressure_On == ON) {
@@ -37,7 +38,7 @@ double Altitude (int Pressure_On){
 double *Acceleration( int Acceleration_On ){
   float x,y,z;
   static double arr[3];
-  
+
   if(Acceleration_On == ON) { 
     IMU.readAcceleration(x,y,z);
     arr[Ax_arr] = x;
@@ -71,7 +72,7 @@ double *Gyroscope( int Gyroscope_On) {
 }
 
 float Temperature( int Temperature_On) {
-  
+
   if(Temperature_On == ON){
     float temp_reading = HTS.readTemperature();
     return temp_reading;
@@ -83,7 +84,7 @@ float Temperature( int Temperature_On) {
 }
 
 float Humidity( int Humidity_On) {
-  
+
   if (Humidity_On == ON){
     float humidity = HTS.readHumidity();
     return humidity;
@@ -102,18 +103,15 @@ float Voltage( double R1 , double R2) {
   float Voltage_Divider = 0.00;
   const float Refference_Voltage = 4.096;
   const float Steps = 1024.0;
-
   Input_Voltage = analogRead(A0);
   Voltage_Out = (Input_Voltage * Refference_Voltage) / (Steps);
   Voltage_Divider = Voltage_Out / (R2 / (R1 + R2) );
-
   return  Voltage_Divider;
-
 }
 */
 
 float *Voltage( int Voltage_On){
-  
+
   float value_1 = 0;
   float value_2 = 0;
   static float arr[2];
@@ -125,7 +123,7 @@ float *Voltage( int Voltage_On){
   arr[V_1_arr] = (value_1 * AR_INTERNAL2V4) / 4095;
   arr[V_2_arr] = (value_2 * AR_INTERNAL2V4) / 4095;
 
-  
+
 
   return arr;
 
@@ -134,4 +132,4 @@ float *Voltage( int Voltage_On){
 
 
 
-#endif  /* Sensors_H */ 
+#endif  /* Sensors_H */
